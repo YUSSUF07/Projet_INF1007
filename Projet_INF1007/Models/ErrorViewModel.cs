a82baeeb8380455e461fe32be43e3a25a090eac8
YUSSUF07 <46982061+YUSSUF07@users.noreply.github.com>
2025-04-02 12:26:23 -0400

    Ajoutez des fichiers projet.

---
+++ Projet_INF1007/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet_INF1007.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
